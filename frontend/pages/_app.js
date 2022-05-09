8a8083dede6d3e0417668d1121082011c60ad262
polycloud-dev <heidenreichdd@gmail.com>
2022-05-09 15:59:50 +0200

    mantine provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,21 @@
+import { MantineProvider } from '@mantine/core'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <MantineProvider
+      withGlobalStyle
+      withNormalizeCSS
+      theme={{
+        'colorScheme': 'dark',
+        colors: {
+          'primary': '#FEFCFB',
+          'secondary': '#38AECC',
+          'background': '#2F323A',
+          'success': '#3BC14A',
+          'error': '#ED254E'
+        }
+      }}
+    ><Component {...pageProps} /></MantineProvider>
 }
 
 export default MyApp
