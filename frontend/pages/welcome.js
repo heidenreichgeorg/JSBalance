78e7abf4bde3b35260d664e7bbaa9182e9ed361a
polycloud-dev <heidenreichdd@gmail.com>
2022-05-16 16:29:25 +0200

    Welcome Drop Page

--- frontend/pages/welcome.js
+++ frontend/pages/welcome.js
@@ -1,40 +1,37 @@
-import { Group, Text } from '@mantine/core';
-import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone'
-import { Photo } from 'tabler-icons-react';
-
-const dropzoneChildren = () => (
-    <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-        <Photo color='gray' size={80} />
-        <div>
-            <Text size="xl" inline>
-                Drag images here or click to select files
-            </Text>
-            <Text size="sm" color="dimmed" inline mt={7}>
-                Attach as many files as you like, each file should not exceed 5mb
-            </Text>
-        </div>
-    </Group>
-);
+import { Group, Text, Title, Center, Space } from '@mantine/core';
+import { Dropzone } from '@mantine/dropzone'
+import { FileCode2 } from 'tabler-icons-react';
+import useLang from '../modules/lang';
 
 export default function WelcomePage() {
+
+    const lang = useLang();
+
     return (
-        <div>
-            <h1>JSBalance</h1>
-            <p>
-                Welcome to JSBalance. This is a simple web application to help you keep track of your
-                finances.
-            </p>
-            <p>
-                Drop your <span>.json</span> File here
-            </p>
-            <Dropzone
-                onDrop={(files) => console.log('accepted files', files)}
-                onReject={(files) => console.log('rejected files', files)}
-                maxSize={3 * 1024 ** 2}
-                accept={'.json'}
-            >
-                {(status) => dropzoneChildren(status)}
-            </Dropzone>
-        </div>
+        <Center
+            style={{ minHeight: '100vh' }}
+        >
+            <div>
+                <Title>{lang.title}</Title>
+                <Text>{lang['upload']}</Text>
+                <Space h="xl" />
+                <Dropzone
+                    onDrop={(files) => console.log('accepted files', files)}
+                    onReject={(files) => console.log('rejected files', files)}
+                    accept={'.json'}
+                    multiple={false}
+                >
+                    {(status) => (
+                        <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
+                            <FileCode2 color='gray' size={80} />
+                            <div>
+                                <Text size="xl" inline>{lang['drag files']}</Text>
+                                <Text size="sm" color="dimmed" inline mt={7}>{lang['upload file']}</Text>
+                            </div>
+                        </Group>
+                    )}
+                </Dropzone>
+            </div>
+        </Center>
     )
 }
