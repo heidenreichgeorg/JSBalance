a8bf3b860656c7c94cbc0f8e5ff2f769da33b130
polycloud-dev <heidenreichdd@gmail.com>
2022-05-16 16:28:48 +0200

    language support

--- frontend/modules/lang.js
+++ frontend/modules/lang.js
@@ -1,22 +1,53 @@
-const content = {}
+import { createContext, useContext } from "react";
 
-export default function useLang(lang='en') {
-    if(!content[lang]) return createLang(lang);
-    return content[lang];
+const LanguageContext = createContext(null);
+
+export default function useLang(lang) {
+
+    const ctx = useContext(LanguageContext);
+
+    if(!ctx) {
+        throw new Error('useLang must be used within a LanguageProvider');
+    }
+
+    const context = ctx.context
+    const language = lang ? lang : ctx.lang
+
+    if(!context[language]) return createLang(language, context);
+    return context[language];
 }
 
-export function createLang(lang,content={}) {
-    content[lang] = new Proxy(content, {
+export function LanguageProvider({children, context={}, lang='en'}) {
+    return (
+        <LanguageContext.Provider value={{'context': createProxy(context), lang}}>
+            {children}
+        </LanguageContext.Provider>
+    )
+}
+
+function createLang(content, lang) {
+    return new Proxy(content, {
         get: function(target, prop) {
             if(!target[prop]) {
                 console.warn(`\x1b[33mMissing translation for '${prop}' in '${lang}'\x1b[0m`);
-                if(lang !== 'en') return content['en'][prop];
+                if(lang !== 'en') {
+                    const en = useLang('en')
+                    return en[prop] || prop;
+                }
+                else return prop;
             }else return target[prop];
         },
         set: function(target, prop, value) {
             target[prop] = value;
             return true;
         }
-    });
-    return content[lang];
+    })
+}
+
+function createProxy(content) {
+    const result = {}
+    Object.keys(content).forEach(key => {
+        result[key] = createLang(content[key], key);
+    })
+    return result;
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,21 +1,29 @@
 import { MantineProvider } from '@mantine/core'
+
+import { LanguageProvider } from '../modules/lang'
+import langContext from '../context/langContext.json'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <MantineProvider
-      withGlobalStyle
-      withNormalizeCSS
-      theme={{
-        'colorScheme': 'dark',
-        colors: {
-          'primary': '#FEFCFB',
-          'secondary': '#38AECC',
-          'background': '#2F323A',
-          'success': '#3BC14A',
-          'error': '#ED254E'
-        }
-      }}
-    ><Component {...pageProps} /></MantineProvider>
+  return <LanguageProvider context={langContext} lang='en'>
+      <MantineProvider
+        withGlobalStyle
+        withNormalizeCSS
+        theme={{
+          'colorScheme': 'dark',
+          colors: {
+            'primary': '#FEFCFB',
+            'secondary': '#38AECC',
+            'background': '#2F323A',
+            'success': '#3BC14A',
+            'error': '#ED254E'
+          }
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </LanguageProvider>
 }
 
 export default MyApp
