c0aafe37a515ffdfb01fbb7348e9e7c3b25e2829
polycloud-dev <heidenreichdd@gmail.com>
2022-05-18 14:50:33 +0200

    merge conflicts

--- start.sh
+++ start.sh
@@ -1,16 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 docker-compose up --build -d
 sh attach.sh
-=======
-cd backend
-<<<<<<< HEAD
-sudo node server.js
->>>>>>> 490764f96fb3774a506b89645204c625a1544ed4
-=======
-sudo node server.js &
->>>>>>> 1052fffaca197bd6c610987325b05d01b4fd6c9d
-=======
-docker-compose up --build -d
-sh attach.sh
->>>>>>> 98a694f4e7af2331b48420ee73854251033e2ff2
