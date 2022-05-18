fa2931dcffc83f263a50dc346b6e96a546812f55
polycloud-dev <heidenreichdd@gmail.com>
2022-05-18 17:56:19 +0200

    added react-query support

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,29 +1,38 @@
+import '../styles/globals.css'
+
+
 import { MantineProvider } from '@mantine/core'
 
 import { LanguageProvider } from '../modules/lang'
 import langContext from '../context/langContext.json'
 
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+ 
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
-  return <LanguageProvider context={langContext} lang='en'>
-      <MantineProvider
-        withGlobalStyle
-        withNormalizeCSS
-        theme={{
-          'colorScheme': 'dark',
-          colors: {
-            'primary': '#FEFCFB',
-            'secondary': '#38AECC',
-            'background': '#2F323A',
-            'success': '#3BC14A',
-            'error': '#ED254E'
-          }
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </LanguageProvider>
+  return (
+  <QueryClientProvider client={queryClient}>
+    <LanguageProvider context={langContext} lang='de'>
+        <MantineProvider
+          withGlobalStyle
+          withNormalizeCSS
+          theme={{
+            'colorScheme': 'dark',
+            colors: {
+              'primary': '#FEFCFB',
+              'secondary': '#38AECC',
+              'background': '#2F323A',
+              'success': '#3BC14A',
+              'error': '#ED254E'
+            }
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </LanguageProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
