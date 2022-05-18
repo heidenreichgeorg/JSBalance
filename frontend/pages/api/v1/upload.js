a4da291f67853faef14d3f3961e25941a5329722
polycloud-dev <heidenreichdd@gmail.com>
2022-05-18 15:43:11 +0200

    added logger and fixed url

--- frontend/pages/api/v1/upload.js
+++ frontend/pages/api/v1/upload.js
@@ -1,23 +1,28 @@
-import isProduction from '../../../modules/isProduction'
+import { backendURL } from '../../../modules/url';
+import logger from '../../../modules/logger';
+
 import fetch from 'node-fetch';
+
+logger.title('UPLOAD API');
 
 export default async function handler(req, res) {
 
-    if(!isProduction()) return res.json({ accepted: true })
-
     const content = req.body
 
-    const apiRes = await fetch('http://backend:81/UPLOAD', {
+    const apiRes = await fetch(backendURL + '/UPLOAD', {
         method: 'POST',
         body: content
     })
+    logger.info('Uploaded file.');
     
     if(apiRes.ok) {
+        logger.log('ok')
         // if success
         res.json({
             accepted: true
         })
     } else {
+        logger.log('not ok')
         // if error
         res.json({
             accepted: false
